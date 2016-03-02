f75e6f1a4f13260b01948a9bf4898062b4b18e9b
Rei Kaneko <rei.kaneko17@gmail.com>
2016-03-03 09:34:59 +1100

    Fixing notification module not showing up

--- main/main/notifications.vb
+++ main/main/notifications.vb
@@ -1,9 +1,6 @@
-﻿Module notifications
-    'Columns in the database : ID | Edit | User | Read
-    'ID: Edit ID
-    'Edit: Edit made
-    'User: User making the change
-    'Read: Whether the notification has been read
+﻿Imports System.Data.OleDb
+
+Module notifications
     Dim notif As Boolean = False 'Whether there are new notifications
 
     Public Sub ping()
@@ -11,8 +8,17 @@
     End Sub 'Change icon if there are new notifications
 
     Public Sub connect()
-
+        Using conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\edit.accdb") 'add your access file to bin\debug and then repalce \athelte with \(name of your file)
+            conn.Open() 'open the connection to the database
+            Using cmd As New OleDbCommand("SELECT edit, user, date, time FROM edits WHERE read = FALSE", conn) '*takes the column with correct rows
+                Using dr = cmd.ExecuteReader() 'reads the database
+                    If dr.HasRows Then 'checks if there are records that fulfill your criteria
+                        Do While dr.Read() 'while loop that goes to eof
+                            '*your function to perform* (access columns with dr(*index*) Or dr(*fieldName*))
+                        Loop
+                    End If
+                End Using
+            End Using
+        End Using
     End Sub 'Connects to edit database and reads fresh edits
-
-
 End Module
