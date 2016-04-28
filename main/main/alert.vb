1745f5c1f1636789fd49df304d85888a63e3bf8a
Rei Kaneko <rei.kaneko17@gmail.com>
2016-04-28 10:05:42 +1000

    Alert Subs Started

---
+++ main/main/alert.vb
@@ -0,0 +1,29 @@
+﻿Module alert
+    Public alertCount As Integer
+
+    Public Sub newAlert(name, change, extraData)
+        Dim alert As String = name + " has "
+        'Open edit database here
+        'Dim date and time from edit database
+        Select Case change
+            Case "add"
+                alert += "added"
+            Case "edit"
+                alert += "edited"
+            Case "delete"
+                alert += "deleted"
+            Case "accAdd"
+                alert += "added account"
+            Case "accEdit"
+                alert += "edited account"
+            Case "accDelete"
+                alert += "deleted account"
+        End Select
+        'Write to edit database using given data
+    End Sub
+
+    Public Sub readAlert()
+        'Open edit dataase here
+        'Get 
+    End Sub
+End Module
