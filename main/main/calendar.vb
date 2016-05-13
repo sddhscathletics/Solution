f195159df8b5a11fad56c1b75dc71ba6bbcb4bb9
CommanderCabbage <linfectiousone@gmail.com>
2016-05-13 11:02:29 +1000

    Add files via upload
    scroll bar oops

--- main/main/calendar.vb
+++ main/main/calendar.vb
@@ -21,4 +21,8 @@
     Public Sub viewEvents(ByVal day As DateTime)
         createEvent.Show()
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        home.Show()
+    End Sub
 End Class
