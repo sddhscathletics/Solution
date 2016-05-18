3668ff96d485fce62017c451a40fa898f40d0eef
Ciaran Farrell <Wafflercraft@gmail.com>
2016-05-18 10:30:37 +1000

    Quick Change

--- main/main/Printing.Designer.vb
+++ main/main/Printing.Designer.vb
@@ -22,8 +22,16 @@ Partial Class Printing
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Printing
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(419, 377)
+        Me.Name = "Printing"
         Me.Text = "Printing"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
