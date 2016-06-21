7b80c914bc02ac0151be4cb295588a923fc366d5
Rei Kaneko <rei.kaneko17@gmail.com>
2016-06-21 10:01:54 +1000

    Started planning the Athlete display

---
+++ main/main/athleteProfile.vb
@@ -0,0 +1,13 @@
+﻿Public Class athleteProfile
+    Private Sub athleteProfile_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub parseAddress(Address)
+        For Each character As Char In Address
+            'Number
+            'Street
+            '
+        Next
+    End Sub
+End Class
