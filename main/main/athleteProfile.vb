2b7305021a6aa5ee6854aadbb2815747987a5ed4
Rei Kaneko <rei.kaneko17@gmail.com>
2016-06-22 11:06:44 +1000

    Parsing complete

--- main/main/athleteProfile.vb
+++ main/main/athleteProfile.vb
@@ -1,40 +1,50 @@
 ﻿Public Class athleteProfile
     Dim adNo, adSt, adSb, adPo As String
-    Private Sub athleteProfile_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Private Sub btnParse_Click(sender As Object, e As EventArgs) Handles btnParse.Click
+        parseNo(txtInput.Text)
+    End Sub
+
+    Private Sub txtInput_Click(sender As Object, e As EventArgs) Handles txtInput.Click
+        txtInput.Clear()
     End Sub
 
     Private Sub parseNo(address As String)
         '5/26 Monmouth St, Randwick 2032
-        Dim i As Integer = 1
+        Dim i As Integer = 0
         While address(i) <> " "
             adNo += address(i)
             i += 1
         End While
+        txtNo.Text = adNo
         parseSt(i, address)
     End Sub
 
-    Private Sub parseSt(i, address)
+    Private Sub parseSt(i As Integer, address As String)
         i += 1
         While address(i) <> ","
             adSt += address(i)
             i += 1
         End While
+        txtSt.Text = adSt
         parseSb(i, address)
     End Sub
 
-    Private Sub parseSb(i, address)
+    Private Sub parseSb(i As Integer, address As String)
         i += 2
         While address(i) <> " "
             adSb += address(i)
             i += 1
         End While
+        txtSb.Text = adSb
+        parsePo(i, address)
     End Sub
 
-    Private Sub parsePo(i, address)
+    Private Sub parsePo(i As Integer, address As String)
         i += 1
-        For k = i To Len(address)
+        For i = i To Len(address) - 1
             adPo += address(i)
         Next
+        txtPo.Text = adPo
     End Sub
 End Class
