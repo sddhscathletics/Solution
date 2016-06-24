1735c6b1518b6a9aaab7d0bb5881f5273716d383
Rei Kaneko <rei.kaneko17@gmail.com>
2016-06-24 11:22:55 +1000

    More progress on athlete profiles.

--- main/main/athleteProfile.Designer.vb
+++ main/main/athleteProfile.Designer.vb
@@ -22,74 +22,57 @@ Partial Class athleteProfile
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.txtInput = New System.Windows.Forms.TextBox()
-        Me.txtNo = New System.Windows.Forms.TextBox()
-        Me.txtSt = New System.Windows.Forms.TextBox()
-        Me.txtPo = New System.Windows.Forms.TextBox()
-        Me.txtSb = New System.Windows.Forms.TextBox()
-        Me.btnParse = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'txtInput
+        'Label1
         '
-        Me.txtInput.Location = New System.Drawing.Point(12, 12)
-        Me.txtInput.Name = "txtInput"
-        Me.txtInput.Size = New System.Drawing.Size(224, 20)
-        Me.txtInput.TabIndex = 0
-        Me.txtInput.Text = "5/26 Monmouth St, Randwick 2032"
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(63, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
         '
-        'txtNo
+        'Label2
         '
-        Me.txtNo.Location = New System.Drawing.Point(12, 38)
-        Me.txtNo.Name = "txtNo"
-        Me.txtNo.Size = New System.Drawing.Size(100, 20)
-        Me.txtNo.TabIndex = 1
-        Me.txtNo.Text = "Number"
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(63, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
         '
-        'txtSt
+        'Label3
         '
-        Me.txtSt.Location = New System.Drawing.Point(12, 64)
-        Me.txtSt.Name = "txtSt"
-        Me.txtSt.Size = New System.Drawing.Size(100, 20)
-        Me.txtSt.TabIndex = 2
-        Me.txtSt.Text = "Street"
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(63, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
         '
-        'txtPo
+        'Label4
         '
-        Me.txtPo.Location = New System.Drawing.Point(12, 116)
-        Me.txtPo.Name = "txtPo"
-        Me.txtPo.Size = New System.Drawing.Size(100, 20)
-        Me.txtPo.TabIndex = 3
-        Me.txtPo.Text = "Postcode"
-        '
-        'txtSb
-        '
-        Me.txtSb.Location = New System.Drawing.Point(12, 90)
-        Me.txtSb.Name = "txtSb"
-        Me.txtSb.Size = New System.Drawing.Size(100, 20)
-        Me.txtSb.TabIndex = 4
-        Me.txtSb.Text = "Suburb"
-        '
-        'btnParse
-        '
-        Me.btnParse.Location = New System.Drawing.Point(118, 38)
-        Me.btnParse.Name = "btnParse"
-        Me.btnParse.Size = New System.Drawing.Size(118, 98)
-        Me.btnParse.TabIndex = 5
-        Me.btnParse.Text = "Parse"
-        Me.btnParse.UseVisualStyleBackColor = True
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(63, 59)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Label4"
         '
         'athleteProfile
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(247, 149)
-        Me.Controls.Add(Me.btnParse)
-        Me.Controls.Add(Me.txtSb)
-        Me.Controls.Add(Me.txtPo)
-        Me.Controls.Add(Me.txtSt)
-        Me.Controls.Add(Me.txtNo)
-        Me.Controls.Add(Me.txtInput)
+        Me.ClientSize = New System.Drawing.Size(1012, 542)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "athleteProfile"
         Me.Text = "athleteProfile"
         Me.ResumeLayout(False)
@@ -97,10 +80,8 @@ Partial Class athleteProfile
 
     End Sub
 
-    Friend WithEvents txtInput As TextBox
-    Friend WithEvents txtNo As TextBox
-    Friend WithEvents txtSt As TextBox
-    Friend WithEvents txtPo As TextBox
-    Friend WithEvents txtSb As TextBox
-    Friend WithEvents btnParse As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
