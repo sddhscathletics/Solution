c1fc949bfd595f28feb8520fde52fd0ca0b9239a
Eddie Belokopytov <eddie_491@hotmail.com>
2016-07-26 09:23:03 +1000

    event Dictionary

--- main/main/calendar.Designer.vb
+++ main/main/calendar.Designer.vb
@@ -22,7 +22,13 @@ Partial Class calendar
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.mnCalendar = New System.Windows.Forms.MonthCalendar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cms = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.AddToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EdtyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.cms.SuspendLayout()
         Me.SuspendLayout()
         '
         'mnCalendar
@@ -32,17 +38,51 @@ Partial Class calendar
         Me.mnCalendar.Name = "mnCalendar"
         Me.mnCalendar.TabIndex = 0
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 232)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'cms
+        '
+        Me.cms.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddToolStripMenuItem, Me.EdtyToolStripMenuItem})
+        Me.cms.Name = "cms"
+        Me.cms.Size = New System.Drawing.Size(153, 70)
+        '
+        'AddToolStripMenuItem
+        '
+        Me.AddToolStripMenuItem.Name = "AddToolStripMenuItem"
+        Me.AddToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AddToolStripMenuItem.Text = "Add"
+        '
+        'EdtyToolStripMenuItem
+        '
+        Me.EdtyToolStripMenuItem.Name = "EdtyToolStripMenuItem"
+        Me.EdtyToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.EdtyToolStripMenuItem.Text = "Edty"
+        '
         'calendar
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(343, 292)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.mnCalendar)
         Me.Name = "calendar"
         Me.Text = "Form1"
+        Me.cms.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents mnCalendar As MonthCalendar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cms As ContextMenuStrip
+    Friend WithEvents AddToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EdtyToolStripMenuItem As ToolStripMenuItem
 End Class
